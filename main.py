8812662f3d4dabb1d1f898e1faa2baeb66b9684e
zhaoyan <154030812@qq.com>
2024-06-27 03:07:14 +0800

    new

---
+++ main.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# 创建一个 Chrome 浏览器实例
+options = webdriver.ChromeOptions()
+options.add_experimental_option('detach', True)  # 不自动关闭浏览器
+browser = webdriver.Chrome(options=options)
+
+# 打开网页
+url = 'https://vip.taoqitu.pro/index.html'  # 替换成你要签到的网站地址
+browser.get(url)
+
+username = '154030812@qq.com'
+password = 'MJ000713_'
+browser.find_element(By.ID, 'regusername').send_keys(username)
+browser.find_element(By.ID, 'regpassword').send_keys(password)
+browser.find_element(By.CLASS_NAME, 'loginbutton').click()
+time.sleep(5)
+browser.find_element(By.CLASS_NAME, 'gonggao_tan_button').click()
+browser.find_element(By.CLASS_NAME, 'qiandao').click()
+time.sleep(5)
+browser.find_element(By.CLASS_NAME, 'invite_get_amount').click()
+
+browser.quit()
